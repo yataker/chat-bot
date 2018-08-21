1053dc2d028d9ab6a7981f00e269d8b601cef6cb
yataker <42265083+yataker@users.noreply.github.com>
2018-08-21 14:18:53 +0900

    index edit

--- index.php
+++ index.php
@@ -1,5 +1,10 @@
 <?php
 
-echo "Hello World"
+//Composerでインストールしたライブグラリを一括読み込み
+reqire_once__Dir__.'vendor/autoload.php';
+
+//POSTメソッドで渡される値を取得、表示
+$inputString = file_get_contents('php://input');
+error_log($inputString);
 
  ?>
