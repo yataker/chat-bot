e1ae3878af472a2884769874960126b6da5b0748
yataker <42265083+yataker@users.noreply.github.com>
2018-08-27 18:27:38 +0900

    動画

--- index.php
+++ index.php
@@ -29,12 +29,12 @@ foreach ($events as $event) {
   //replyLocationMessage($bot, $event->getReplyToken(), 'LINE', '東京都渋谷区渋谷2-21-1 ヒカリエ27階', 35.659025, 139.703473);
 
   // スタンプを返信
-  replyStickerMessage($bot, $event->getReplyToken(), 1, 1);
+  //replyStickerMessage($bot, $event->getReplyToken(), 1, 1);
 
   // 動画を返信
-  //replyVideoMessage($bot, $event->getReplyToken(),
-  //  'https://' . $_SERVER['HTTP_HOST'] . '/videos/sample.mp4',
-  //  'https://' . $_SERVER['HTTP_HOST'] . '/videos/sample_preview.jpg');
+  replyVideoMessage($bot, $event->getReplyToken(),
+    'https://' . $_SERVER['HTTP_HOST'] . '/videos/sample.mp4',
+    'https://' . $_SERVER['HTTP_HOST'] . '/videos/sample_preview.jpg');
 
   // オーディファイルを返信
   //replyAudioMessage($bot, $event->getReplyToken(), 'https://' . $_SERVER['HTTP_HOST'] . '/audios/sample.m4a', 6000);
