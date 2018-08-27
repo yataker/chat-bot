e1a522a77c2ae23bb5bd0929085d3d4e38728baf
yataker <42265083+yataker@users.noreply.github.com>
2018-08-27 17:31:10 +0900

    ソース更新

--- index.php
+++ index.php
@@ -3,23 +3,20 @@
 // Composerでインストールしたライブラリを一括読み込み
 require_once __DIR__ . '/vendor/autoload.php';
 
-
-//アクセストークンを使いCurlHTTPClientをインスタンス化
-$httpClinet = new \LINE\LINEBot\HTTPClient\CurlHTTPCLient(getenv('CHANNEL_ACCESS_TOKEN'));
-
-//CurlHTTPCLientとシークレットを使いLINEBotをインスタンス化
-$bot = new \LINE\LINEBot($httpClinet,['channelSecret' => getenv('CHANNEL_SECRET')]);
-
-//LINE Mesaging API リクエストに付与した署名を取得
+// アクセストークンを使いCurlHTTPClientをインスタンス化
+$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient(getenv('CHANNEL_ACCESS_TOKEN'));
+// CurlHTTPClientとシークレットを使いLINEBotをインスタンス化
+$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => getenv('CHANNEL_SECRET')]);
+// LINE Messaging APIがリクエストに付与した署名を取得
 $signature = $_SERVER['HTTP_' . \LINE\LINEBot\Constant\HTTPHeader::LINE_SIGNATURE];
 
-//署名が正当がどうかチェック。正当であれば、パースし配列へ
-$events = $bot->parseEventRequest(file_get_cntents('php://input'),$signature);
+// 署名が正当かチェック。正当であればリクエストをパースし配列へ
+$events = $bot->parseEventRequest(file_get_contents('php://input'), $signature);
 
 //配列に格納された各イベントをループで処理
 foreach ($events as $events) {
   // テキストを返信
-  $bot->replyText($event->getPeplyToken(),'TextMessage')
+  $bot->replyText($event->getReplyToken(),'TextMessage');
 }
 
  ?>
